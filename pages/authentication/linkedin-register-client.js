9695e78def0fa3e3219a81e96b4f72772d7ccb2b
Darwin H.C <darwinhace@gmail.com>
2021-04-17 12:42:34 +0700

    add authentication

---
+++ pages/authentication/linkedin-register-client.js
@@ -0,0 +1,5 @@
+import LinkedinChecker from "~components/pages/Authentication/LinkedinChecker";
+
+const LinkedInRegisterClient = () => <LinkedinChecker />;
+
+export default LinkedInRegisterClient;
---
+++ pages/authentication/linkedin-register-writer.js
@@ -0,0 +1,5 @@
+import LinkedinChecker from "~components/pages/Authentication/LinkedinChecker";
+
+const LinkedInRegisterWriter = () => <LinkedinChecker />;
+
+export default LinkedInRegisterWriter;
